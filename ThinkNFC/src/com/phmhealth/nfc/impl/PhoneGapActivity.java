a673cc833891482852ffd5b3a94d2f270f9ae362
AshrayShetty <ashray@thinking-code.com>
2013-03-18 18:06:19 +0530

    First cut of the Phonegap-NFC Plugin

---
+++ ThinkNFC/src/com/phmhealth/nfc/impl/PhoneGapActivity.java
@@ -0,0 +1,15 @@
+package com.phmhealth.nfc.impl;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+
+public class PhoneGapActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 super.setIntegerProperty("loadUrlTimeoutValue", 60000);
+		super.loadUrl("file:///android_asset/www/app.html");
+	}
+}
